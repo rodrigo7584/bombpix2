4fcc1d5c24085b1cef95f23443bd6ef785aa9a31
rodrigo7584 <rodrigo.gandhi@cojiba.com.br>
2024-03-16 18:07:11 -0300

    16/03/2024

---
+++ js/script.js
@@ -0,0 +1,85 @@
+/*MENU MOBILE */
+const btnMobile = document.querySelector('button.btn-mobile-menu')
+function toggleMenu() {
+  const navMobile = document.querySelector('header.menu nav')
+  btnMobile.classList.toggle('active')
+  navMobile.classList.toggle('active')
+}
+btnMobile.addEventListener('click', toggleMenu)
+
+/*MENU MOBILE */
+/*ACCORDION */
+const accordion = document.querySelectorAll('.accordion')
+
+accordion.forEach(item => {
+  const question = item.querySelector('.question')
+  const answer = item.querySelector('.answer')
+
+  answer.style.maxHeight = 0
+
+  question.addEventListener('click', () => {
+    if (item.classList.contains('open')) {
+      answer.style.maxHeight = 0
+      item.classList.remove('open')
+    } else {
+      answer.style.maxHeight = answer.querySelector('.content').scrollHeight + 'px'
+      item.classList.add('open')
+    }
+  })
+})
+/*END ACCORDION */
+/*LOADER */
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    // Quando a página estiver totalmente carregada, esconde o loader
+    document.querySelector('.loader-wrapper').style.display = 'none'
+  }
+}
+/*END LOADER */
+
+/*MODAL DIALOG */
+function initModalDialog() {
+  const modalButtons = document.querySelectorAll('[data-button-modal]')
+
+  if (modalButtons.length) {
+    modalButtons.forEach(button => {
+      const modal = document.querySelector(`[data-dialog-modal="${button.dataset.buttonModal}"]`)
+
+      button.addEventListener('click', () => {
+        modal.classList.add('fade-in')
+        modal.showModal()
+      })
+
+      function fadeOut() {
+        modal.classList.add('fade-out')
+        setTimeout(() => {
+          modal.close()
+          modal.classList.remove('fade-out')
+        }, 1000 * 0.3)
+      }
+
+      const buttonsClose = modal.querySelectorAll('[data-dialog-modal="modal-close"]')
+      buttonsClose.forEach(buttonClose => {
+        buttonClose.addEventListener('click', () => {
+          fadeOut()
+        })
+      })
+
+      modal.addEventListener('click', e => {
+        const modalDimensions = modal.getBoundingClientRect()
+        if (
+          e.clientX < modalDimensions.left ||
+          e.clientX > modalDimensions.right ||
+          e.clientY < modalDimensions.top ||
+          e.clientY > modalDimensions.bottom
+        ) {
+          fadeOut()
+        }
+      })
+    })
+  }
+}
+initModalDialog()
+/*END MODAL DIALOG */
+
+AOS.init()
